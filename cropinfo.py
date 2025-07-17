7241815c5b6235a4a4ae4ecae253851e3089bcb3
waseemghazigithub <159131359+waseemghazigithub@users.noreply.github.com>
2025-07-17 18:01:11 +0500

    default.py

--- cropinfo.py
+++ cropinfo.py
@@ -37,6 +37,7 @@ def get_connection():
             f"UID={st.secrets['DB_USER']};"
             f"PWD={st.secrets['DB_PASSWORD']};"
             "TrustServerCertificate=yes;"
+            "Encrypt=yes;"
             "Connection Timeout=30;"
         )
         return conn
